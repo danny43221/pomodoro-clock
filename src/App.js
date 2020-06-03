f4b97111605df3f0346dffd8a29a88f575b4e4fa
Daniel <dtues003@fiu.edu>
2020-06-02 22:49:51 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PomodoroClock from './containers/PomodoroClock/PomodoroClock'
+
+export const App = () => {
+  return (
+    <div>
+      <PomodoroClock />
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/containers/PomodoroClock/PomodoroClock.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import classes from "./PomodoroClock.module.css";
+
+const PomodoroClock = () => {
+	
+	return (
+		<div className={classes.PomodoroClock}>
+			<h1>Pomodoro Clock</h1>
+		</div>
+	);
+};
+
+export default PomodoroClock;
